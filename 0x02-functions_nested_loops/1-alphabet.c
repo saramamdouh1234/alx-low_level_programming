0b6801bb21f1be39c3b516e196767836b005dd46
Sarah <sarah.m.alawady@gmil.com>
2023-07-18 14:29:06 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_alphabet - make the alphabet
+*
+* Return: viod 
+*/
+
+void print_alphabet(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}

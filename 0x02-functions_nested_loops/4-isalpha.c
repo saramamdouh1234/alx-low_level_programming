b63ea3b178d046d9c8b53e4edcb963be4c272f0d
Sarah <sarah.m.alawady@gmil.com>
2023-07-18 15:51:42 -0700

    s

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
-  * _isalpha - checks for lowercase or uppercase character
-  * Returns 1 if c is a letter, lowercase or uppercase
-  * Returns 0 otherwise
-  * @c: the character in ASCi table
-  * Return: 1 for lowercase. 0 for others
-  */
-
+ * _isalpha - checks for lowercase or uppercase character
+ * Returns 1 if c is a letter, lowercase or uppercase
+ * Returns 0 otherwise
+ * @c: the character in ASCi table
+ * Return: 1 for lowercase. 0 for uppercase
+ */
 int _isalpha(int c)
 {
 if (c >= 97 && c <= 122)

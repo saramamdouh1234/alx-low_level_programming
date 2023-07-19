984e3887e06ca66c3cb23277e5b6f5e4d944fe10
Sarah <sarah.m.alawady@gmil.com>
2023-07-18 18:00:27 -0700

    s

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add- Accs two values
+ * @n1: The fitst number to be computed
+ * @n2: The second number to be computed
+ * Return: Integer vlue
+ */
+
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}

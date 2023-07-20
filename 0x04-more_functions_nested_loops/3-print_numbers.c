a5f37105389ea5be04f150b799e24c64fde30dfd
Sarah <sarah.m.alawady@gmil.com>
2023-07-20 11:45:50 -0700

    most_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0-9
- * only using _putchar twice
- * Return: Always 0
- */
+* print_numbers - print the number since 0 to 9
+* Return: the number since 0 to 9
+*/
+
 void print_numbers(void)
 {
-	int num = 0;
-
-	do {
-		_putchar(num + 48);
-		num++;
-	} while (num >= 0 && num <= 9);
+	int n;
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
 	 _putchar('\n')
 }

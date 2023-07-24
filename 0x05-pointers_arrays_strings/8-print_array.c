ecab264b3ba0027243a3f3ecd98064492be968e5
Sarah <sarah.m.alawady@gmil.com>
2023-07-24 16:47:14 -0700

    print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -13,9 +14,9 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (i == 0)
-			printf("%d", a[i]);
+			printf(",%d", a[i]);
 		else
 			printf(", %d", a[i]);
 	}
-		printf("\n");
+		printf('\n');
 }

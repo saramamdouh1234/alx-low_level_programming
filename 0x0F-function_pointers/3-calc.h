41906a0bde815dd59017c0f277538cab0a34c142
Sarah <sarah.m.alawady@gmil.com>
2023-08-16 15:31:34 -0700

    a

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,5 @@
-#ifndef CALC_H
-#define CALC-H
-
+#ifndef _HEADER_
+#define _HEADER_
 /**
  * struct op - Struct op
  *
@@ -19,5 +18,4 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-
 #endif

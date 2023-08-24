df6a0c6295ae449d0f384187b8e93324acef4f1a
sarah mamdouh <sarah.m.alawady@gmail.com>
2023-08-24 15:43:52 -0700

    a

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,50 +1,46 @@
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end of a list_t list
- * @head: pointer to pointer of first node of a list_t list
- * @str: string to be included in node
- * Return: address of the new element or NULL if it fails
- */
+ * add_node_end - Adds a new node at the end of a list
+ *@head: pointer to the head of the list
+ *@str: string to be added to the list
+ *
+ *Return: NULL in case of failure
+ *or address or the element
+*/
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
-	list_t *current;
-
-	current = *head;
+	char *dup;
+	int len;
+	list_t *new, *last;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
-	new->str = strdup(str);
-	new->len = _strlen(str);
+	dup = strdup(str);
+	if (str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
 	new->next = NULL;
 
 	if (*head == NULL)
 		*head = new;
 	else
 	{
-		while (current->next != NULL)
-			current = current->next;
-
-		current->next = new;
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
 	}
-
-	return (new);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string to be evaluated
- * Return: length of string
- */
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-
-	return (i);
+	return (*head);
 }

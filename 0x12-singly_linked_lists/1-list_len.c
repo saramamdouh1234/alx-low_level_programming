82c48e6a27e0516ad74f96ddcc5deca9a38b5a13
sarah mamdouh <sarah.m.alawady@gmail.com>
2023-08-24 15:14:15 -0700

    a

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+  * list_len - finds the number of nodes in a list
+  * @h: singly linked list
+  * Return: number of elements
+  */
+size_t list_len(const list_t *h)
+{
+	int j;
+
+	for (j = 0; h; j++)
+		h = h->next;
+	return (j);

8fff3925d0dfa1fb856efbaed21a67c19a64b903
sarah mamdouh <sarah.m.alawady@gmail.com>
2023-09-04 14:02:43 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,41 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
+ * append_text_to_file - Appends text to the end of a file.
+ * @filename: The name of the file.
+ * @text_content: The NULL-terminated string to append.
  *
- * Return: 1 if it success. -1 if it fails.
+ * Return: 1 on success, -1 on failure.
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+    int fd, write_status, close_status;
+    ssize_t text_length = 0;
 
-	if (!filename)
-		return (-1);
+    if (filename == NULL)
+        return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+    fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (fd == -1)
-		return (-1);
+    if (fd == -1)
+        return (-1);
 
-	if (!text_content)
-		text_content = "";
+    if (text_content != NULL)
+    {
+        while (text_content[text_length] != '\0')
+            text_length++;
 
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
+        write_status = write(fd, text_content, text_length);
+        if (write_status == -1)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
 
-	rwr = write(fd, text_content, nletters);
+    close_status = close(fd);
+    if (close_status == -1)
+        return (-1);
 
-	if (rwr == -1)
-		return (-1);
-
-	close(fd);
-
-	return (1);
+    return (1);
 }

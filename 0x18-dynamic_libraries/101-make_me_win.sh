47f0020c3d2f210b0e3eb8ca3174d17f4f8c82a7
sarah mamdouh <sarah.m.alawady@gmail.com>
2023-10-16 02:46:42 -0700

    win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp ../sh2.sh .
-. ./sh2.sh
+wget -P .. https://raw.githubusercontent.com/saramamdouh1234/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"

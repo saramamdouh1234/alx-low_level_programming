cae7166b2d5de49d2cc978de1c1d05db9add6566
sarah mamdouh <sarah.m.alawady@gmail.com>
2023-11-03 12:29:35 -0700

    new

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implementation  djb2 algorithm
+ * @str: value in function
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; 
+	}
+	return (hash);
+}

c891bc22b9c389a4a2961d61976a4824d02015c6
sarah mamdouh <sarah.m.alawady@gmail.com>
2023-11-03 13:31:13 -0700

    new

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Get a value.
+ * @ht: Hash table.
+ * @key: Key.
+ * Return: the value associated with the element,
+ * or NULL if not  found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *cur;
+
+	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	cur = ht->array[index];
+	if (cur == NULL)
+		return (NULL);
+	while (strcmp(cur->key, key) && cur != NULL)
+	{
+		cur = cur->next;
+	}
+	if (cur == NULL)
+		return (NULL);
+	else
+		return (cur->value);
+}
